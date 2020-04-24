08c4f5b5a7c5db424ddff50f34c6c34e4854ad99
Cody Garn <garn.cody@gmail.com>
2020-04-24 12:54:02 -0500

    complete

---
+++ src/ducks/store.js
@@ -0,0 +1,6 @@
+import valuesReducer from './valuesReducer'
+import {createStore} from 'redux'
+import {devToolsEnhancer} from 'redux-devtools-extension'
+
+
+export default createStore(valuesReducer, devToolsEnhancer())
